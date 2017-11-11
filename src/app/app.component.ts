12d54274d29d2d50b5bdd2284f04a7be5481e4fd
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-12 01:18:25 +0530

    OoS - Custom Observable
    This is Out of Scope (OoS) for this testing demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,27 @@ export class AppComponent {
       (number: number) => {
         console.log(number);
       }
+    );
+
+    const myObservable = Observable.create( (observer: Observer<string>) => {
+      setTimeout(observer.next('first package')
+        , 3000);
+      setTimeout(observer.next('second package')
+        , 4000);
+      // After error package, nothing will get sent, even complete package
+      /* setTimeout(observer.error('error package')
+        , 5000); */
+      setTimeout(observer.complete()
+        , 6000);
+      // This package won't get sent as the observer is already marked as complete
+      setTimeout(observer.next('fourth package')
+        , 7000);
+    } );
+
+    myObservable.subscribe(
+      (msg: string) => {console.log(msg);},
+      (error: string) => {console.log(error);},
+      () => {console.log('completed');}
     );
   }
 }
