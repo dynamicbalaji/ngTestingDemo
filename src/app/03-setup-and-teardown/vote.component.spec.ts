43f642ab777a7bb48b57371a61ce25b42afc4c17
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-11 23:50:20 +0530

    Testing a component
    AAA - Arrange, Act & Assert
    
    Setup - beforeEach, beforeAll
    Teardown - afterEach, afterAll

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,27 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    // Arrange
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment totalVotes when upVoted', () => {
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downVoted', () => {
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
