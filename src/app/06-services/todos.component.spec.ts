b3c49e0258d106a713141f0a462c004df4189e74
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-12 19:23:34 +0530

    Working with spies
    spyOn for mocking a service method

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,29 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should call getTodos method and initialize values for todos', () => {
+    let todos = [1,2,3];
+    // Changing the implementation of getTodos method
+    // spyOn(object, 'methodName')
+    spyOn(service,'getTodos').and.callFake(() => {
+      return Observable.from([ todos ]);
+    });
+
+    component.ngOnInit();
+
+    //expect(component.todos.length).toBeGreaterThan(0);
+    //expect(component.todos.length).toBe(3);
+    expect(component.todos).toBe(todos);
   });
 });
